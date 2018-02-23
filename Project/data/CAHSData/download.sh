3ede46df1143911ece9940ddf4ea2f3328b7d11a
Neal Marquez <nmmarquez@protonmail.com>
2018-02-23 09:09:05 -0800

    update HS download file

--- Project/data/CAHSData/download.sh
+++ Project/data/CAHSData/download.sh
@@ -8,16 +8,22 @@ for i in {1994..2015..1}
 	i_short=$(echo ${i:2:4})
 	if [ $i -lt 2003 ]
 	then
-	    curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
-	    curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=GradsEth&cPage=filesgrad.asp" > senior_$i.txt
+	    echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp"
+	    echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=GradsEth&cPage=filesgrad.asp"
+	    wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
+	    wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i_short$ni_short&cCat=GradsEth&cPage=filesgrad.asp" > senior_$i.txt
 
 	elif [ $i -lt 2006 ]
 	then
-	    curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
-            curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradsEth&cPage=filesgrad.asp" > senior_$i.txt
+	    echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp" 
+        echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradsEth&cPage=filesgrad.asp"
+	    wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradsEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
+        wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradsEth&cPage=filesgrad.asp" > senior_$i.txt
 
-        else
-	    curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
-            curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradEth&cPage=filesgrad.asp" > senior_$i.txt
-        fi
+    else
+        echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradEth&cPage=filesgradaf.asp"
+        echo "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradEth&cPage=filesgrad.asp"
+        wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=UCGradEth&cPage=filesgradaf.asp" > grad_elig_$i.txt
+        wget --user-agent=Safari  -O - "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=School&cYear=$i-$ni_short&cCat=GradEth&cPage=filesgrad.asp" > senior_$i.txt
+    fi
 done
